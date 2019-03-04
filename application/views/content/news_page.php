c713f7c62efe17c457548b1e89e5f7fab7d76da8
saifulsaif <saifulsaif5854@gmail.com>
2019-03-04 17:54:31 +0600

    image resize

--- application/views/content/news_page.php
+++ application/views/content/news_page.php
@@ -15,7 +15,7 @@
     <tr>
         <td><a href="<?= base_url() ?>news/show_one/<?= $n['ne_id']; ?>"><?= $n['ne_title']; ?></a></td>
         <td><?= implode(' ', array_slice(explode(' ', $n['ne_desc']), 0, 15));; ?></td>
-        <td ><img src="<?php echo base_url(); ?>global/uploads/<?= $n['ne_img']; ?>"/>
+        <td><img style="height: 150px;" src="<?php echo base_url(); ?>global/uploads/<?= $n['ne_img']; ?>"/>
         </td>
     </tr>
     </tbody>
